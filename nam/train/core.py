3e2e8f27d72716cad64f58a3a4e10e064ac427ba
38github <18294149+38github@users.noreply.github.com>
2026-08-29 10:24:08 +0200

    Enable shuffling in training dataloader

--- nam/train/core.py
+++ nam/train/core.py
@@ -924,7 +924,7 @@ def _get_configs(
     learning_config = {
         "train_dataloader": {
             "batch_size": batch_size,
-            "shuffle": False,
+            "shuffle": True,
             "pin_memory": True,
             "drop_last": True,
             "num_workers": 4,
